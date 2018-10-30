c24b6e29d91f45685dc2fb111fb3bb0ec7481e11
maataro <masahiro.infinite77@gmail.com>
2018-10-30 20:00:31 +0000

    テンプレートに出欠情報を対応させる

--- routes/schedules.js
+++ routes/schedules.js
@@ -99,7 +99,7 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
             })
           })
 
-          console.log(availabilityMapMap);  // TODO 除去する
+          
 
           res.render('schedule', {
             user: req.user,
