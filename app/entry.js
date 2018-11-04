23fe81ebe63968fe9b34a76fc43de56cab245b9f
maataro <masahiro.infinite77@gmail.com>
2018-11-04 08:26:49 +0000

    予定編集フォームとハンドラの実装

--- app/entry.js
+++ app/entry.js
@@ -11,10 +11,10 @@ $('.availability-toggle-button').each((i, e) => {
     const nextAvailability = (availability + 1) % 3;
     $.post(`/schedules/${scheduleId}/users/${userId}/candidates/${candidateId}`,
       { availability: nextAvailability },
-      (data) => {
-        button.data('availability', data.availability);
+      (data) => {        
+        button.data('availability', data.availability);  // 実行結果を受け取って button 要素の、 data-* 属性を更新
         const availabilityLabels = ['欠', '？', '出'];
-        button.text(availabilityLabels[data.availability]);
+        button.text(availabilityLabels[data.availability]);  // ボタンのラベルを更新
       });
   });
 });
